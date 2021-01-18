e20e84f9114d256f1aa4fbeba74e574339539d38
Anton Liubimov <ant.liubimov@gmail.com>
2021-01-18 14:24:16 +0500

    finished part3

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const homeRoutes = require('./routes/home')
+const cardRoutes = require('./routes/card')
 const coursesRoutes = require('./routes/courses')
 const addRoutes = require('./routes/add')
 
@@ -16,12 +17,13 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use(express.urlencoded({extended: true}))
 app.use('/',homeRoutes)
 app.use('/courses',coursesRoutes)
-app.use('/add',addRoutes)
+app.use('/add', addRoutes)
+app.use('/card', cardRoutes)
 
 
 
