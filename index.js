a2db3878feb6cd1eb4088753a0a9f29a2b9da811
Anton Liubimov <ant.liubimov@gmail.com>
2021-01-19 16:07:45 +0500

    finished part4

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 const express = require('express');
+const Handlebars = require('handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 const path = require('path');
+const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
 const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
 const coursesRoutes = require('./routes/courses')
 const addRoutes = require('./routes/add')
+const ordersRoutes = require('./routes/orders')
+const User = require('./models/user')
 
 const app = express()
 
 const hbs = exphbs.create({
   defaultLayout: 'main',
-  extname: 'hbs'
+  extname: 'hbs',
+  handlebars: allowInsecurePrototypeAccess(Handlebars)
 })
 
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs')
 app.set('views', 'views')
+
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById('600697fa5cb3010f591a9d17')
+    req.user = user
+    next()
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 app.use(express.static(path.join(__dirname,'public')));
 
@@ -24,11 +40,35 @@ app.use('/',homeRoutes)
 app.use('/courses',coursesRoutes)
 app.use('/add', addRoutes)
 app.use('/card', cardRoutes)
-
-
+app.use('/orders', ordersRoutes)
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on the port ${PORT}`)
-})
+async function start() {
+  try {
+    const url = `mongodb+srv://antliubim:Ye5DC9ZBBwSbLQKA@cluster0.ixcl0.mongodb.net/shop`;
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    })
+    const candidate = await User.findOne()
+    if (!candidate) {
+      const user = new User({
+        email: 'antliubimov@gmail.com',
+        name: 'Anton',
+        cart: {items: []}
+      })
+      await user.save()
+    }
+
+    app.listen(PORT, () => {
+      console.log(`Server running on the port ${PORT}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start();
+
